63d96438962d2d67e1706371d95bc43ddb4bfb6b
Jonathan-Taylor319 <jontaylor319@gmail.com>
2025-05-03 12:03:49 -0400

    added placeholders to check dynamic rendering

--- Honest_Auto_Care/src/App.jsx
+++ Honest_Auto_Care/src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import { useState } from 'react';
 import TopBar from './components/TopBar';
 import Home from './components/Home';
-// import About from './components/About';
-// import Services from './components/Services';
-// import Contact from './components/Contact';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
 
 function App() {
   const [activePage, setActivePage] = useState('home');
