473b1222a7fee0ad1c2c25a510d3e88fef4bddac
Jonathan-Taylor319 <jontaylor319@gmail.com>
2025-05-05 11:11:31 -0400

    updates to services

--- Honest_Auto_Care/src/components/Services.jsx
+++ Honest_Auto_Care/src/components/Services.jsx
@@ -1,8 +1,18 @@
+import wrenches from "../assets/wrenches.png"
+import CEL from "../assets/Cel.png"
+
 export default function Services() {
 
     return (
         <>
-        
+            <h1 style={{textAlign:"center"}}>Below are some of the services we offer:</h1>
+            <div>
+                <div className="TopPics">
+                    <img src={wrenches} className="icon"></img>
+                    <img src={CEL} className="icon"></img>
+                </div> 
+               
+            </div>
         </>
     )
 }
